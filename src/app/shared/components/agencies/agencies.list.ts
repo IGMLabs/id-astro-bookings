1027a4c5554235a9f21bcd22000b7bb0d4bba1dc
ivan doce <idocrod@gmmail.com>
2022-06-08 12:48:11 +0200

    feat: container presenter

--- src/app/shared/components/agencies/agencies.list.ts
+++ src/app/shared/components/agencies/agencies.list.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AgenciesApi } from 'src/app/core/api/agencies.api';
 import { Agency } from 'src/app/core/api/agency.interface';
 
 @Component({
@@ -7,34 +8,19 @@ import { Agency } from 'src/app/core/api/agency.interface';
   styleUrls: ['./agencies.list.css'],
 })
 export class AgenciesList implements OnInit {
-  public agencies:Agency[]= [
-    {
-      id: 'space-y',
-      name: 'Space Y',
-      range: 'Interplanetary',
-      status: 'Active',
-    },
-    {
-      id: 'green-origin',
-      name: 'Green Origin',
-      range: 'Orbital',
-      status: 'Active',
-    },
-    {
-      id: 'virgin-way',
-      name: 'Virgin Way',
-      range: 'Orbital',
-      status: 'Pending',
-    },
-  ];
+@Input() public agencies:Agency[]=[];
+@Output() private  reload=new EventEmitter();
 
   public reloading = false;
 
-  constructor() {}
+  constructor(agenciesApi:AgenciesApi) {
+    this.agencies=agenciesApi.getAll();
+  }
 
-  public reload(list: string) {
+  public onReloadClick(list: string) {
     this.reloading = true;
     console.log('Reloading...' + list);
+    this.reload.emit();
   }
 
   public getAgenciesLength() {
